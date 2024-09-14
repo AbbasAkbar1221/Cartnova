d7e3e8d0a2f49eb05f8b7e7d22f68cae8a1f7df0
AbbasAkbar1221 <abbasakbar765@gmail.com>
2024-09-15 01:17:26 +0530

    Add routes and link to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Shop from './components/Pages/Shop';
+import Cart from './components/Pages/Cart';
+import Contact from './components/Pages/Contact';
+import ShopCategory from './components/Pages/ShopCategory';
+import LoginSignup from './components/Pages/LoginSignup';
+import Product from './components/Pages/Product';
 
 function App() {
   return (
-    <>
+    <div className='App'>
+    <Router>
     <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/men' element={<ShopCategory category='Men'/>} />
+        <Route path='/women' element={<ShopCategory category='Women'/>} />
+        <Route path='/kids' element={<ShopCategory category='Kids'/>} />
+        <Route path='/product/:id' elemente={<Product/>} />
+        <Route path='/login' element={<LoginSignup/>} />
+      </Routes>
+    </Router>
+    <div className="content">
     <h1 className='font-bold text-4xl'>Welcome to Cartnova</h1>
-    </>
+    </div>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    let [menu, setMenu] =useState('shop');
+  let [menu, setMenu] = useState("shop");
   return (
     <nav>
       <div className="flex items-center justify-between h-20 px-5 bg-gray-900">
@@ -15,11 +16,36 @@ const Navbar = () => {
         {/* Center Section */}
         <div className="flex-1 flex justify-center">
           <ul className="flex space-x-10 text-white">
-            <li className="hover:text-gray-300 cursor-pointer" onClick={()=> setMenu('shop')}>Shop All {menu ==='shop'?<hr/> : <></>}</li>
-            <li className="hover:text-gray-300 cursor-pointer" onClick={()=> setMenu('men')} >Shop Men {menu ==='men'?<hr/> : <></>}</li>
-            <li className="hover:text-gray-300 cursor-pointer" onClick={()=> setMenu('women')}>Shop Women {menu ==='women'?<hr/> : <></>}</li>
-            <li className="hover:text-gray-300 cursor-pointer" onClick={()=> setMenu('kids')}>Shop Kids {menu ==='kids'?<hr/> : <></>}</li>
-            <li className="hover:text-gray-300 cursor-pointer" onClick={()=> setMenu('contact')}>Contact {menu ==='contact'?<hr/> : <></>}</li>
+            <li
+              className="hover:text-gray-300 cursor-pointer"
+              onClick={() => setMenu("shop")}
+            >
+              <Link to='/'>Shop All</Link> {menu === "shop" ? <hr /> : <></>}
+            </li>
+            <li
+              className="hover:text-gray-300 cursor-pointer"
+              onClick={() => setMenu("men")}
+            >
+              <Link to='/men'>Shop Men</Link> {menu === "men" ? <hr /> : <></>}
+            </li>
+            <li
+              className="hover:text-gray-300 cursor-pointer"
+              onClick={() => setMenu("women")}
+            >
+              <Link to='/women'>Shop Women</Link> {menu === "women" ? <hr /> : <></>}
+            </li>
+            <li
+              className="hover:text-gray-300 cursor-pointer"
+              onClick={() => setMenu("kids")}
+            >
+              <Link to='/kids'>Shop Kids</Link> {menu === "kids" ? <hr /> : <></>}
+            </li>
+            <li
+              className="hover:text-gray-300 cursor-pointer"
+              onClick={() => setMenu("contact")}
+            >
+              <Link to='/contact'>Contact</Link> {menu === "contact" ? <hr /> : <></>}
+            </li>
           </ul>
         </div>
 
@@ -29,10 +55,10 @@ const Navbar = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
-            Signup
+            <Link to='/login'>Signup</Link>
           </button>
           <div className="flex items-center">
-            <img src="./shopping-cart.png" alt="cart" className="h-8" />
+            <Link to='/cart'><img src="./shopping-cart.png" alt="cart" className="h-8" /></Link>
           </div>
         </div>
       </div>
