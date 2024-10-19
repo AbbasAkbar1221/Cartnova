3b1b3fc3ba0a7905bf054caf2b5d44f9d06275a6
AbbasAkbar1221 <abbasakbar765@gmail.com>
2024-10-20 01:13:51 +0530

    correct alignment

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Item = ({ ItemObj }) => {
-  const handleClick=()=>{
+  const handleClick = () => {
     window.scrollTo({
-      top:0,
-      behavior:'smooth'
-    })
-  }
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
+    <Link to={`/product/${ItemObj.id}`} onClick={handleClick}>
     <div className="border-2 border-gray-400 rounded-lg p-4 flex flex-col items-center w-full transition-transform transform hover:scale-105 animate-fadeIn">
-      <Link to={`/product/${ItemObj.id}`} onClick={handleClick}>
+      
         <img
           src={ItemObj.image}
           alt={ItemObj.name}
           className="w-full h-96 object-cover mb-4 rounded-lg transition-transform transform hover:scale-110"
         />
         <h1 className="text-xl font-semibold mb-2">{ItemObj.name}</h1>
+
         <div className="cost flex flex-col items-center">
           <div className="newCost text-lg font-bold text-red-600 mb-1">
             ₹{ItemObj.new_price}
@@ -26,8 +27,9 @@ const Item = ({ ItemObj }) => {
             ₹{ItemObj.old_price}
           </div>
         </div>
-        </Link>
+      
     </div>
+    </Link>
   );
 };
 
